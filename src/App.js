4b65b8384a7f8c7323f1b66dc423ae79fd51431f
João Pedro <jps12carvalho@gmail.com>
2021-05-11 19:29:21 -0300

    Adicionando funçao de edit distance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function editDistance(a, b){
+  if (a.length == 0) return b.length
+  if (b.length == 0) return a.length
+
+  let matrix = []
+
+  let i, j;
+
+  for (i=0; i<=a.length; i++){
+    matrix[i] = [i]
+  }
+
+  for (j=0; j<=b.length; j++){
+    matrix[0][j] = j;
+  }
+
+  for (i = 1; i<=a.length; i++){
+    for (j = 1; j <= b.length; j++){
+      if (a[i-1] === b[j-1]){
+        matrix[i][j] = matrix[i-1][j-1]
+      }
+      else {
+        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1,
+                        Math.min(
+                          matrix[i-1][j] + 1, 
+                          matrix[i][j-1] + 1
+                        ))
+      }
+    }
+  }
+  return matrix[a.length][b.length]
+}
+
+function filterByDistance(options, value){
+  
+}
 
 function App() {
+
+  const [value, setValue] = useState("")
+  const [options, setOptions] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={() => setValue()}></input>
+      <text>{editDistance("Algo", "Auge")}</text>
     </div>
   );
 }
